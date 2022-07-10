ece4f043dfd1caaf13855d6d88f4d6e99157eaf4
t <t>
2022-07-10 13:49:00 +0900

    コメントの加筆

--- Snake/1.py
+++ Snake/1.py
@@ -3,22 +3,27 @@ from pygame.locals import QUIT
 import sys
 
 pygame.init()
+# 画面の大きさを600 * 600 ピクセルに設定
 SURFACE = pygame.display.set_mode((600,600))
+# 時間の設定
 FPSCLOCK = pygame.time.Clock()
 
-
-def paint():
+# 画面の描画
+def display_paint():
+    # 画面を真っ黒にする
     SURFACE.fill((0,0,0))
-
+    # 画面を更新する
     pygame.display.update()
 
 def main():
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
+                # pygameの終了
                 pygame.quit()
+                # プログラムの終了
                 sys.exit()
-        paint()
+        display_paint()
         FPSCLOCK.tick(5)
 
 if __name__ == "__main__":
