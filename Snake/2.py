7dfe7879c74a6c594a2a894e7ef6af05bd9bdd0d
t <t>
2022-07-10 14:21:45 +0900

    コメント加筆

--- Snake/2.py
+++ Snake/2.py
@@ -3,26 +3,32 @@ from pygame.locals import QUIT
 import sys
 
 pygame.init()
+# 画面の大きさを600 * 600 ピクセルに設定
 SURFACE = pygame.display.set_mode((600,600))
+# 時間の設定
 FPSCLOCK = pygame.time.Clock()
 
-
-def paint():
+# 画面の描画
+def display_paint():
+    # 画面を真っ黒にする
     SURFACE.fill((0,0,0))
     for index in range(20):
         # 縦線が描かれる
         pygame.draw.line(SURFACE, (64, 64, 64), (index*30, 0),(index*30, 600))
         # 横線が描かれる
         pygame.draw.line(SURFACE, (64, 64, 64), (0, index*30),(600, index*30))
+    # 画面を更新する
     pygame.display.update()
 
 def main():
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
+                # pygameの終了
                 pygame.quit()
+                # プログラムの終了
                 sys.exit()
-        paint()
+        display_paint()
         FPSCLOCK.tick(5)
 
 if __name__ == "__main__":
